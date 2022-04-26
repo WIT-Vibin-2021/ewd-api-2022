7635b0c24b330e5d57b9627696130a675f3f2a38
Vibin Varghese <91533254+WIT-Vibin-2021@users.noreply.github.com>
2022-04-26 18:16:57 +0100

    Configured Linting

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import http from 'http';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 
-dotenv.config()
+dotenv.config();
 
-const port = process.env.PORT
+const port = process.env.PORT;
 // Configure our HTTP server to respond with Hello World to all requests.
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
